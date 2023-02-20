5406042bcf01b58db807a84890b03d7ca26af4fc
ivicakartelo <ivicakartelo@users.noreply.github.com>
2023-02-21 00:10:46 +0100

    remove some import

--- src/components/BlogPostsMenuLeft.js
+++ src/components/BlogPostsMenuLeft.js
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
-import { useLayoutEffect } from 'react'
-import { useRef } from 'react'
 
 function BlogPostsMenuLeft({ blogposts }) {
 
